79123ccf66463275c7d2f3c1828e4e7da02abb92
vigneshuniverse <founder.vigneshuniverse@gmail.com>
2026-08-01 04:44:55 +0000

    Update Config.js

--- Assets/Js/Config.js
+++ Assets/Js/Config.js
@@ -3,9 +3,9 @@
  * After Deploying Google Apps Script, Paste Your Web App URL Below.
  * Example: 'https://script.google.com/macros/s/AKfycb.../exec'
  */
-window.APPS_SCRIPT_URL = '';
+window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwIbe6oq8YDRYizikWAZjEKK5ZMMCl8jmrvCyd_jYW6MTuDot7dVOSQ9Ns0QZtCJry9Ow/exec';
 /**
  * Admin Panel URL (Same Apps Script Deployment)
  * After Deploy, Paste The URL Here. The Admin Link On Homepage Will Open It.
  */
-window.ADMIN_PANEL_URL = '';
+window.ADMIN_PANEL_URL = 'https://script.google.com/macros/s/AKfycbwIbe6oq8YDRYizikWAZjEKK5ZMMCl8jmrvCyd_jYW6MTuDot7dVOSQ9Ns0QZtCJry9Ow/exec';
